e60205f86405262269fa29ae835135b0901ae785
kudligi <nihal1011ml@gmail.com>
2017-10-27 19:55:10 +0530

    (commit#2 by gangsta)

--- database/server.py
+++ database/server.py
@@ -5,9 +5,9 @@ from pottery import RedisList as redList
 from pottery import NextId
 
 #connection with redis-py client 
-dir_con = red(host = 'localhost',port = 6379,db = 0)
+dir_con = red(host = '13.126.191.137',port = 6379,db = 0, password = 'dKoO2KDuqSrD')
 #connection with pottery client 
-pot_con = red.from_url('http://localhost:6379/')
+pot_con = red.from_url('redis://:dKoO2KDuqSrD@13.126.191.137:6379/')
 
 #string encode with b prefix
 def bytify(strung):
@@ -21,6 +21,42 @@ def byte2int(strung):
 
 def byte2float(strung):
 	return float(byte2string(strung))
+
+'''
+User: pottery dictionary
+-name
+-userId(primaryKeycebook facebook generated)
+-phone no
+-lat
+-long
+-decoded address
+-active order list
+-bool subscribed
+-list of blacklisted restaurants
+'''
+def addUser(body):
+	key = "user:"+str(body['id'])+":details"
+	user = redDict(redis = pot_con, key = key)
+	user['name']=body['name']
+	user['lat']=body['lat']
+	user['long']=body['long']
+	user['subscribed']=body['subscribed']
+	user[]=body[] 
+
+d = {'id':23,"name":"nihal"}
+
+#addUser(d)
+
+'''
+Restaurant: pottery dictionary
+-name
+-restId(primaryKey and artificial key)
+-active offer list
+-phone no list
+-delivery Radius
+-extra dets
+'''
+
 
 
 
