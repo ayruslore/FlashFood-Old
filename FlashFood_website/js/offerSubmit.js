d54689471b2e6d538070a6206067e47425196590
Akshay Pant <Akshay.Pant@iiitb.org>
2017-11-03 14:24:52 +0530

    Offer page updated

--- FlashFood_website/js/offerSubmit.js
+++ FlashFood_website/js/offerSubmit.js
@@ -1,10 +1,20 @@
 function submitOffers(){
-  var dish=document.getElementsByName('dish');
-  var quantity=document.getElementsByName('quantity');
-  var origp=document.getElementsByName('origp');
-  var offp=document.getElementsByName('offp');
+  var dish=document.getElementsByName('dish')[0].value;
+  var quantity=document.getElementsByName('quantity')[0].value;
+  var origp=document.getElementsByName('origp')[0].value;
+  var offp=document.getElementsByName('offp')[0].value;
   var acc=document.getElementsByName('acc');
-  for(var i=0; i<dish.length; i++){
-    console.log(dish[i].value, quantity[i].value, origp[i].value, offp[i].value);
+
+  if(quantity<=0 | origp<=0 | offp<=0 | origp<offp){
+    alert("Invalid Values");
+    return;
   }
+
+  var offer={"dish": dish, "quantity": quantity, "original_price": origp, "offer_price": offp};
+  if(acc[0].value.length!=0){
+    offer["accompaniment"]=[];
+    for(var i=0; i<acc.length; i++)
+      offer["accompaniment"].push(acc[i].value);
+  }
+  console.log(JSON.stringify(offer));
 }
