9af813b05ac70057e789635bf1dca7c7eb7ed0f1
Surya Srikar Peruri <suryasrikar1997@gmail.com>
2017-11-05 00:36:12 +0530

    api calls post request updated

--- database/server.py
+++ database/server.py
@@ -1,12 +1,35 @@
+import bottle
+from bottle import get, post, request, run, install, response  # or route
+from gevent import monkey; monkey.patch_all()
+
 from redis import Redis as red
 from pottery import RedisDict as redDict
 from pottery import RedisSet as redSet
 from pottery import RedisList as redList
 from pottery import NextId
 
-#connection with redis-py client 
+#Enable Cors to send/recieve data.
+class EnableCors(object):
+    name = 'enable_cors'
+    api = 2
+
+    def apply(self, fn, context):
+        def _enable_cors(*args, **kwargs):
+            # set CORS headers
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+            response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+            if bottle.request.method != 'OPTIONS':
+                # actual request; reply with the actual response
+                return fn(*args, **kwargs)
+
+        return _enable_cors
+
+
+#connection with redis-py client
 dir_con = red(host = '13.126.191.137',port = 6379,db = 0, password = 'dKoO2KDuqSrD')
-#connection with pottery client 
+#connection with pottery client
 pot_con = red.from_url('redis://:dKoO2KDuqSrD@13.126.191.137:6379/')
 
 rest_ids = NextId(key='rest-ids', masters={pot_con})
@@ -36,18 +59,22 @@ User: pottery dictionary
 -bool subscribed
 -list of blacklisted restaurants
 '''
-#/addUser
-def addUser(body):
-	key = "user:"+str(body['id'])+":details"
-	user = redDict(redis = pot_con, key = key)
-	for key in body:
-		user[key] = body[key] 
+
+@post('/adduser')  #post request to add the user details.
+def addUser():
+    body = request.json
+    key = "user:" + str(body['id']) + ":details"
+    user = redDict(redis = pot_con, key = key)
+    for key in body:
+        user[key] = body[key]
 
 #d = {'id':23,"name":"nihal"}
 #addUser(d)
 
 #/updateUser
-def updateUser(body):
+@post('/updateuser')
+def updateUser():
+    body = request.json
 	key = "user"+str(body["id"])+":details"
 	user = redDict(redis = pot_con, key = key)
 	for key in body:
@@ -68,15 +95,18 @@ Restaurant: pottery dictionary
 -delivery Radius
 -extra dets
 '''
-
-def addRestaurant(body):
+@post('/addrest')
+def addRestaurant():
+    body = request.json
 	rest_id = next(rest_ids)
 	key = "rest:"+str(rest_id)+":details"
 	rest = redDict(redis = pot_con, key = key)
 	for key in body:
 		rest[key] = body[key]
 
-def updateRestaurant(body):
+@post('/updaterest')
+def updateRestaurant():
+    body = request.json
 	key = "rest:"+str(body["id"])+":details"
 	rest = redDict(redis = pot_con, key = key)
 	for key in body:
@@ -95,10 +125,11 @@ Offers
 -offerPrice
 '''
 
+'''
 def addOffer(body):
 
 def claimOffer(body):
-
+'''
 
 '''
 Orders
@@ -112,3 +143,6 @@ Orders
 -
 
 '''
+
+install(EnableCors())
+run(host='0.0.0.0', port=3000, debug=True, server='gevent')
